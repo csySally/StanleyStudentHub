4971fe57e8cd1850788c3380fedc706f76468736
Sally Chen <23687599@student.uwa.edu.au>
2025-06-10 15:21:40 +0800

    home page done

--- src/components/Discover.jsx
+++ src/components/Discover.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/discover.css";
+import oval1 from "../assets/images/oval1.png";
+import oval2 from "../assets/images/oval2.png";
 
 function Discover({ cards }) {
   return (
-    <div className="discover-container">
-      {cards.map((card, index) => (
-        <div key={index} className="discover-card-item">
-          <div
-            style={{ backgroundImage: `url(${card.image})` }}
-            className="discover-card-item-image"
-          >
-            <div className="discover-card-item-image-mask"></div>
-            <Link to={card.url}>{card.title}</Link>
+    <div className="discover">
+      <div className="discover-container">
+        {cards.map((card, index) => (
+          <div key={index} className="discover-card-item">
+            <div
+              style={{ backgroundImage: `url(${card.image})` }}
+              className="discover-card-item-image"
+            >
+              <div className="discover-card-item-image-mask"></div>
+              <Link to={card.url}>{card.title}</Link>
+            </div>
+            <div className="discover-card-item-content"></div>
           </div>
-          <div className="discover-card-item-content"></div>
-        </div>
-      ))}
+        ))}
+      </div>
+      <img src={oval1} alt="" className="oval1" />
+      <img src={oval2} alt="" className="oval2" />
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,9 +14,12 @@ import StudentRepre from "../components/StudentRepre";
 import QAList from "../components/Q&A/QAList";
 import backgroundPic from "../assets/images/home-background.jpeg";
 import slogan from "../assets/images/slogan.png";
-
+import oval3 from "../assets/images/oval3.png";
+import oval4 from "../assets/images/oval4.png";
+import career from "../assets/images/career.png";
 import {
   homeSlides,
+  homeSlides2,
   homeOuterLink,
   EventCards,
   DiscoverCards,
@@ -64,21 +67,99 @@ function Home() {
       <div className="discover-cards-container">
         <Discover cards={DiscoverCards} />
       </div>
-
       <SecondHeading
         title="Need Immediate Assistance?"
+        backgroundColor="#fff"
+        textColor="#00007B"
+        toMore={false}
+      />
+      <InfoCard homeCardData={homeCardData} />
+      <Heading
+        title="Learn"
+        backgroundColor="#00007B"
+        textColor="#fff"
+        toMore={false}
+      />
+      <SwiperCom slides={homeSlides2} />
+      <SecondHeading
+        title="Quick Access"
+        backgroundColor="#00007B"
+        textColor="#fff"
+        toMore={false}
+      />
+      <div className="accessContent">
+        <p>
+          Need to check your timetable or access course materials? Use the links
+          below to jump straight to your key systems.
+        </p>
+      </div>
+      <div className="home-outerlink-container">
+        <Outerlink outerLink={homeOuterLink} />
+        <img src={oval3} alt="" className="oval3" />
+      </div>
+
+      <SecondHeading
+        title="Career Development"
+        backgroundColor="#00007B"
+        textColor="#fff"
+        toMore={false}
+      />
+      <div className="career">
+        <div className="career-img">
+          <img src={career} />
+        </div>
+        <div className="career-content">
+          <p>
+            Our Employment Team is here to assist you with your placement needs.
+            Whether you're seeking internships, work experience, or job
+            placements, we’re here to help you every step of the way.
+          </p>
+          <Link to="./wbt-support" className="career-link">
+            {">>> More"}
+          </Link>
+        </div>
+      </div>
+      <div className="enjoy">
+        <Heading
+          title="Enjoy"
+          backgroundColor="#fff"
+          textColor="#00007B"
+          toMore={false}
+        />
+        <p>
+          At Stanley College, we celebrate diversity and foster an inclusive
+          learning environment.We encourage you to participate in student clubs,
+          societies, and volunteer initiatives to expand your network, make new
+          friends, and develop a sense of belonging within the college
+          community.
+        </p>
+        <img src={oval4} alt="" className="oval4" />
+        <img src={oval4} alt="" className="oval5" />
+      </div>
+
+      <SecondHeading
+        title="Meet Our Student Representatives"
         backgroundColor="#fff"
         textColor="#00007B"
         toMore={true}
         toLink="/important-contacts"
       />
-      <InfoCard homeCardData={homeCardData} />
-      <div className="home-outerlink-container">
-        <Outerlink outerLink={homeOuterLink} />
-      </div>
       <StudentRepre studentRepresentative={studentRepresentative} />
-      <QAList qaList={qaList} />
-      <div>
+      <Heading
+        title="FAQs"
+        backgroundColor="#00007B"
+        textColor="#fff"
+        toMore={false}
+      />
+      <div className="faqs">
+        <div className="faqs-content">
+          Find answers to the questions we get asked the most about studying at
+          Stanley College.
+        </div>
+        <QAList qaList={qaList} />
+      </div>
+
+      {/*       <div>
         <ul>
           <li>
             <Link to="/it-support">IT Support</Link>
@@ -102,7 +183,7 @@ function Home() {
             <Link to="/requests-and-feedbacks">Requests and Feedbacks</Link>
           </li>
         </ul>
-      </div>
+      </div> */}
 
       <Footer />
     </div>
