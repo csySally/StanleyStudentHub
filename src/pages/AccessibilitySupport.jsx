a2597a2b7a2ffbd9b63a2cdbfc493eac03752b96
Sally Chen <23687599@student.uwa.edu.au>
2025-06-13 13:44:08 +0800

    page done

--- src/pages/AccessibilitySupport.jsx
+++ src/pages/AccessibilitySupport.jsx
@@ -3,8 +3,22 @@ import Header from "../components/Header";
 import Welcome from "../components/Welcome";
 import Footer from "../components/Footer";
 import ArrowHeading from "../components/ArrowHeading";
+import Heading from "../components/Heading.jsx";
+import StuffCard from "../components/StuffCard.jsx";
+import SecondHeading from "../components/SecondHeading";
 import backgroundPic from "../assets/images/accessibility-background.jpeg";
-import { arrowHeadings } from "../config/accessibilityContentConfig.js";
+import {
+  arrowHeadings,
+  heading1,
+  careCont,
+  careStuff,
+  secondH1,
+  external,
+  externalCardData,
+} from "../config/accessibilityContentConfig.js";
+import ellipse from "../assets/images/Ellipse.png";
+import "../styles/accessibilitySupport.css";
+import InfoCard from "../components/InfoCard.jsx";
 
 function AccessibilitySupport() {
   return (
@@ -14,9 +28,30 @@ function AccessibilitySupport() {
         backgroundPic={backgroundPic}
         welcomeText="Accessibility Support"
       />
-      <h1>Accessibility Support</h1>
+      <Heading
+        title={heading1}
+        backgroundColor="#00007B"
+        textColor="#fff"
+        toMore={false}
+      />
       <ArrowHeading arrowHeadings={arrowHeadings[0]} />
-
+      <div className="care-container">
+        <div className="care-container-p">
+          <p>{careCont}</p>
+        </div>
+        <div className="care-container-card">
+          <StuffCard stuffCard={careStuff} showAll={false} />
+        </div>
+      </div>
+      <SecondHeading
+        title={secondH1}
+        backgroundColor="#fff"
+        textColor="#00007B"
+        toMore={false}
+      />
+      <div className="external-content">{external}</div>
+      <img src={ellipse} className="ellipse" />
+      <InfoCard cardData={externalCardData} />
       <Footer />
     </div>
   );
