c305e1f74057c8bf769f2a3967c59ea0e1f134e8
Sally Chen <23687599@student.uwa.edu.au>
2025-06-16 13:02:50 +0800

    itSupport Page Layout

---
+++ src/config/ITContentConfig.js
@@ -0,0 +1,64 @@
+import star from "../assets/icons/star.svg";
+import thalia from "../assets/images/thalia.png";
+
+export const heading1 = "Technical Assistance";
+
+export const heading2 = "FAQs";
+
+export const arrowHeadings = [
+  {
+    firstImg: star,
+    firstCont: "Something not working? ",
+    secondCont: "We’ve got your back!e!",
+  },
+];
+
+export const ITCont = `Stanley College Perth provides robust IT support to ensure that you have the technical assistance you need.
+Our Services Include:
+Technical Assistance: Help with software installation, troubleshooting, and technical issues.
+Online Learning Support: Assistance with accessing and using online learning platforms and resources.
+IT Workshops: Workshops on using college IT resources, cybersecurity, and digital literacy.
+`;
+
+export const ITStuff = [
+  {
+    title: "IT Support Team",
+    name: "itsupport@stanleycollege.edu.au  ",
+    campus: "(08) 6371 9999 ",
+    image: thalia,
+  },
+];
+
+export const qaList = [
+  {
+    question: "What is the best way to study for a test?",
+    answer:
+      "The best way to study for a test is to study the material and practice the material. So, you can go to the school website and find the material and practice the material. And you can also ask your teacher for help. Don't forget to ask your teacher for help if you need it. If you need more information, you can go to the school website and find the material and practice the material.",
+    button: "Ask Teacher",
+    link: "https://www.google.com",
+  },
+  {
+    question: "What do I do if I am sick?",
+    answer:
+      "If you are sick, please stay home and rest. If you are feeling unwell, please contact your teacher or the school nurse.",
+  },
+  {
+    question: "How do I request timetable changes?",
+    answer:
+      "If you need to request a timetable change, please contact your teacher or the school office.",
+    button: "View More",
+    link: "https://www.google.com",
+  },
+  {
+    question: "Can I change my course?",
+    answer:
+      "Yes, you can change your course. Please contact the school office to request a course change. And some of the courses are available on the school website.",
+  },
+  {
+    question: "What if I forget to pay my fees?",
+    answer:
+      "If you forget to pay your fees, please contact the school office to pay your fees. And some of the fees are due on the first day of the term. Policy and procedure is available on the school website.",
+    button: "Go to Dashboard",
+    link: "https://www.google.com",
+  },
+];
